e2630c656471f61f4a42c80a3f756bf2edce53b6
Subh B <algobuddha@gmail.com>
2025-07-08 20:13:42 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+
+app.get('/questions', (req, res) => {
+  fs.readFile(path.join(__dirname, 'questions.json'), 'utf-8', (err, data) => {
+    if (err) {
+      return res.status(500).send('Error reading questions.json');
+    }
+    res.json(JSON.parse(data));
+  });
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'question.html'));
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
