5265e1082e797e0b0e493f1e41e9d99f5ce4da1c
Subh B <algobuddha@gmail.com>
2025-07-08 21:02:47 +0530

    add public folder

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/questions', (req, res) => {
   fs.readFile(path.join(__dirname, 'questions.json'), 'utf-8', (err, data) => {
